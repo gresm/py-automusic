99e83f19862593f5fa4bff2c2d5e4c33cf34e0c3
gresm <78505251+gresm@users.noreply.github.com>
2022-03-01 12:00:00 +0100

    + structures/   ~ __init__.py   ~ base_note.py

--- structures/__init__.py
+++ structures/__init__.py
@@ -2,3 +2,4 @@ from .base_instrument import *
 from .configs import *
 from .base_note import *
 from .base_instrument import *
+from .bundled_structures import *
--- structures/base_note.py
+++ structures/base_note.py
@@ -1,15 +1,21 @@
-from abc import ABC, abstractmethod
 from pydub import AudioSegment
 
+from . import AbstractInstrument
 
-class AbstractNote(ABC):
-    time: float
 
-    @abstractmethod
+class BaseNote:
+    time: int
+    octave: float
+
+    def __init__(self, instrument: AbstractInstrument, octave: float = 1, time: int = 1):
+        self.instrument = instrument
+        self.time = time
+        self.octave = octave
+
     def get_sound(self) -> AudioSegment:
-        pass
+        return self.instrument.get_sound(self.octave, self.time)
 
 
 __all__ = [
-    "AbstractNote"
+    "BaseNote"
 ]
