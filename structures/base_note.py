8f5222f0cb293a7876cc3da92bf83579f4c57f0f
gresm <78505251+gresm@users.noreply.github.com>
2022-03-02 12:00:00 +0100

    ~ structures/   ~ base_note.py   ~ bundled_structures.py

--- structures/base_note.py
+++ structures/base_note.py
@@ -1,21 +1,40 @@
+from abc import ABC, abstractmethod
 from pydub import AudioSegment
 
 from . import AbstractInstrument
 
 
-class BaseNote:
+class AbstractNote(ABC):
+    @abstractmethod
+    def __init__(self, instrument: AbstractInstrument):
+        self.instrument = instrument
+
+    @abstractmethod
+    def get_sound(self) -> AudioSegment:
+        pass
+
+    @abstractmethod
+    def get_lasting_time(self) -> int:
+        pass
+
+
+class Note(AbstractNote):
     time: int
     octave: float
 
     def __init__(self, instrument: AbstractInstrument, octave: float = 1, time: int = 1):
-        self.instrument = instrument
+        super(Note, self).__init__(instrument)
         self.time = time
         self.octave = octave
 
     def get_sound(self) -> AudioSegment:
         return self.instrument.get_sound(self.octave, self.time)
 
+    def get_lasting_time(self) -> int:
+        return self.time
+
 
 __all__ = [
-    "BaseNote"
+    "AbstractNote",
+    "Note"
 ]
--- structures/bundled_structures.py
+++ structures/bundled_structures.py
@@ -1,5 +1,7 @@
+from __future__ import annotations
+
 from pydub import AudioSegment, generators
-from . import AbstractInstrument
+from . import AbstractInstrument, AbstractNote, Note
 
 
 class SinInstrument(AbstractInstrument):
@@ -7,6 +9,43 @@ class SinInstrument(AbstractInstrument):
         return generators.Sine(self.configs.starting_pitch).to_audio_segment(self.configs.note_speed)
 
 
+class PauseNote(AbstractNote):
+    time: int
+
+    def __init__(self, instrument: AbstractInstrument, time: int):
+        super(PauseNote, self).__init__(instrument)
+        self.time = time
+
+    def get_sound(self) -> AudioSegment:
+        return AudioSegment.silent(self.time)
+
+    def get_lasting_time(self) -> int:
+        return self.time
+
+
+class MultiNote(AbstractNote):
+    time: int
+
+    def __init__(self, instrument: AbstractInstrument, time: int, note: Note, notes: list[Note]):
+        super(MultiNote, self).__init__(instrument)
+        self.time = time
+        self.note = note
+        self.notes = notes
+
+    def get_sound(self) -> AudioSegment:
+        sound = self.note.get_sound()
+
+        for note in self.notes:
+            sound = sound.overlay(note.get_sound())
+
+        return sound
+
+    def get_lasting_time(self) -> int:
+        return self.time
+
+
 __all__ = [
-    "SinInstrument"
+    "SinInstrument",
+    "PauseNote",
+    "MultiNote"
 ]
