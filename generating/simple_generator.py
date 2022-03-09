67f93a35e8fc948136866526507702e4529e2cfb
gresm <78505251+gresm@users.noreply.github.com>
2022-03-09 12:00:00 +0100

    ~ generating/ ~ simple_generator.py ~ structures/ ~ configs.py

--- generating/simple_generator.py
+++ generating/simple_generator.py
@@ -1,11 +1,67 @@
-from . import AbstractGenerator
+from __future__ import annotations
+
+from . import AbstractGenerator, AbstractInstrument, MusicBar, NotePromise, Note
+
+import random as rd
 
 
-class SimpleRandomGenerator:
-    pass
+class SimpleRandomGenerator(AbstractGenerator):
+    def __init__(self, instrument: AbstractInstrument, max_detail: float, pitch_range: tuple[int, int]):
+        self.instrument = instrument
+        self.configs = self.instrument.configs
+        self.max_detail = max_detail
+        self.pitch_range = pitch_range
+
+    def _beep_generate(self, value: float):
+        if value <= self.max_detail:
+            return value
+        option = rd.randint(0, 3)
+        if option == 0:
+            return value
+        elif option == 2:
+            if value / 2 <= self.max_detail:
+                return value
+            v1 = self._beep_generate(value / 2)
+            v2 = self._beep_generate(value / 2)
+
+            if not isinstance(v1, list):
+                v1 = [v1]
+
+            if not isinstance(v2, list):
+                v2 = [v2]
+
+            return v1 + v2
+        else:
+            if value / 2 <= self.max_detail:
+                return value
+            v1 = self._beep_generate(value / 2)
+            v2 = self._beep_generate(value / 2)
+
+            if not isinstance(v1, list):
+                v1 = [v1]
+
+            if not isinstance(v2, list):
+                v2 = [v2]
+
+            return v2 + v1
+
+    def _generate_beeps(self) -> list[float]:
+        ret = self._beep_generate(self.configs.time_per_tact)
+
+        if not isinstance(ret, list):
+            return [ret]
+        return ret
+
+    def _generate_pitch(self):
+        return rd.randint(0, self.pitch_range[1]) + self.pitch_range[0]
+
+    def generate_chunk(self) -> MusicBar:
+        bar = MusicBar(self.configs, self.instrument)
+        for beep in self._generate_beeps():
+            bar.add_note(NotePromise(Note, beep, octave=self._generate_pitch()))
+        return bar
 
 
 __all__ = [
     "SimpleRandomGenerator"
 ]
-
--- structures/configs.py
+++ structures/configs.py
@@ -6,10 +6,11 @@ class Configs:
     time_per_tact: float
     note_speed: int
     starting_pitch: int
+    pitch_detail: int
 
 
 def default_configs():
-    return Configs(time_per_tact=4, note_speed=1000, starting_pitch=200)
+    return Configs(time_per_tact=4, note_speed=1000, starting_pitch=200, pitch_detail=50)
 
 
 __all__ = [
